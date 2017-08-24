eacf14944ea65a61961d33c6357df4a1618bd3f9
Juliana Moraes <juliana@fapesb.ba.gov.br>
2017-08-24 19:19:54 -0300

    all files

---
+++ src/main/java/com/juliana/testjava/TestJavaApplication.java
@@ -0,0 +1,12 @@
+package com.juliana.testjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestJavaApplication.class, args);
+	}
+}
